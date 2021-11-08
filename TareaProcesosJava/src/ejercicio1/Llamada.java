10ca0b1fd5124cbcc2d3601fa9145c3a8e57e46c
Mikel <Mikel@ASUS-MIKEL>
2021-11-08 03:31:09 +0100

    Actualizacion trabajo en casa

--- TareaProcesosJava/src/ejercicio1/Llamada.java
+++ TareaProcesosJava/src/ejercicio1/Llamada.java
@@ -1,5 +1,11 @@
 package ejercicio1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*Crea un programa en Java en el package "ejercicio1" que lea de teclado dos números y 
  * muestre por la salida estándar la división del primero entre el segundo. El programa 
  * ha de controlar todos los errores que se puedan producir y sacarlos por la salida de 
@@ -11,8 +17,37 @@ package ejercicio1;
 public class Llamada {
 
 	public static void main(String[] args) {
-		int numero1 = 
+		Runtime run = Runtime.getRuntime();
+		Process p = null;
+		try {
+			p = run.exec("cmd /c start /wait java -cp bin ejercicio1.Division");
+			p.waitFor();
+			System.out.println("aqui");
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedWriter bw = new BufferedWriter(new FileWriter("salida.txt"));
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				System.out.println(linea);
+				bw.write(linea);
+			}
+			br.close();
+		} 
+		catch (InterruptedException ie) {
+			ie.printStackTrace();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				System.err.println(linea);
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
